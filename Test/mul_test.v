7cf1065fb9bf9b3df4dcba7f5df08cb435ff3355
majilokesh <72696170+majilokesh@users.noreply.github.com>
2023-08-06 18:20:59 +0530

    Update mul_test.v

--- Test/mul_test.v
+++ Test/mul_test.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 31.05.2021 16:23:35
-// Design Name: 
-// Module Name: tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module mul_test;
 
